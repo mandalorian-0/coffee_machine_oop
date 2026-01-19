d55abac1673d50748694424407a4f92651d087f2
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-19 00:54:30 -0800

    Revamp final

---
+++ main.py
@@ -0,0 +1,33 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+def main():
+    is_on = True
+    options = menu.get_items()
+
+    while is_on:
+        choice = input(f"What would you like? ({options})")
+
+        if choice == "report":
+            coffee_maker.report()
+            money_machine.report()
+        elif choice == "off":
+            is_on = False
+            continue
+        else:
+            drink = menu.find_drink(choice)
+
+            if(coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost)):
+                coffee_maker.make_coffee(drink)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
